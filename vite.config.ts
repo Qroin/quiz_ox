574d1552d27d2daf45c2a9ac3fa3b52b577a453e
Qroin <62214298+Qroin@users.noreply.github.com>
2025-07-13 01:30:10 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/quiz_ox/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
